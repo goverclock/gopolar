70c97092442611508b92021039ec73daa1d95fc9
goverclock <goverclock@outlook.com>
2024-03-07 14:26:02 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ go install ./cmd/...
 cd webui
 npm install
 vite build
-rm -rf $GOPATH/bin/gpwebui
-mv ./dist $GOPATH/bin/gpwebui
+sudo rm -rf $GOPATH/bin/gpwebui
+sudo mv ./dist $GOPATH/bin/gpwebui
